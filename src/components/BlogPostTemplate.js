a011d94692136266705071e7d349b9f22176c84c
JRobsonJr <jrobsonjr16@gmail.com>
2019-10-09 10:50:44 -0300

    Update blog post template

--- src/components/BlogPostTemplate.js
+++ src/components/BlogPostTemplate.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import Header from './Header';
+
+import './BlogPostTemplate.css';
 
 const BlogPostTemplate = ({ data }) => {
     const { markdownRemark } = data;
     const { frontmatter, html } = markdownRemark;
     return (
-        <div className="blog-post-container">
-            <div className="blog-post">
-                <h1>{frontmatter.title}</h1>
-                <h6>{frontmatter.date}</h6>
-                <div
-                    className="blog-post-content"
-                    dangerouslySetInnerHTML={{ __html: html }}
-                />
+        <>
+            <Header />
+            <div className="layout">
+                <div className="blog-post-container">
+                    <div className="blog-post">
+                        <p className="blog-post-date">{frontmatter.date}</p>
+                        <h1>{frontmatter.title}</h1>
+                        <div
+                            className="blog-post-content"
+                            dangerouslySetInnerHTML={{ __html: html }}
+                        />
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
