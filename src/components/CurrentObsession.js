959a6c2bd6c47a95d4646d2854a9453a09280f88
JRobsonJr <jrobsonjr16@gmail.com>
2019-10-12 21:44:56 -0300

    Create CurrentObsession component

---
+++ src/components/CurrentObsession.js
@@ -0,0 +1,31 @@
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import './CurrentObsession.css';
+
+const CurrentObsession = () => (
+    <div className="current-obssession-container">
+        <div className="footer-content">
+            <h2 className="text-light text-center">Current obsession</h2>
+            <p className="text-light text-center m-0">
+                <i>
+                    I'm high enough from all the waiting to ride a wave on your
+                    inhaling.
+                </i>
+            </p>
+            <p className="text-light text-center">
+                — Imogen Heap, Guy Sighsworth (Frou Frou)
+            </p>
+            <iframe
+                className="responsive-iframe"
+                src="https://www.youtube.com/embed/5XONarRhvI4"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+            ></iframe>
+        </div>
+    </div>
+);
+
+export default CurrentObsession;
