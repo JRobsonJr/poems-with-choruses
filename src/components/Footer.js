469366145dad1673124fec9360251d789af96d6f
JRobsonJr <jrobsonjr16@gmail.com>
2019-10-12 21:45:23 -0300

    Create page footer

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = () => (
+    <footer>
+        <ul className="footer-link-list list-inline text-center text-light m-0">
+            <li>
+                <a
+                    className="footer-link"
+                    href="https://last.fm/user/jrobsonjr"
+                >
+                    LAST.FM
+                </a>
+            </li>
+            <li>
+                <a className="footer-link" href="https://twitter.com/JRosboJr">
+                    TWITTER
+                </a>
+            </li>
+            <li>
+                <a className="footer-link" href="https://github.com/jrobsonjr">
+                    GITHUB
+                </a>
+            </li>
+        </ul>
+    </footer>
+);
+
+export default Footer;
