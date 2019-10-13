c5271ebdd94b87a37ec0363125005f900dd51430
JRobsonJr <jrobsonjr16@gmail.com>
2019-10-12 21:45:42 -0300

    Update header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,14 @@ import './Header.css';
 const Header = () => (
     <header>
         <div className="header-content">
-            <h1 className="text-center m-0">
+            <h1 className="header-blog-title text-center">
                 <Link className="header-link" to="/">
                     Poems With Choruses
                 </Link>
             </h1>
+            <p className="header-blog-description text-center text-light m-0">
+                Documenting life as I try to fit it in the lyrics of a song.
+            </p>
         </div>
     </header>
 );
