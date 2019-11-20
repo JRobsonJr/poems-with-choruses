f9ed4ec62f36afa4bf020e0bd6713f33d03a1247
JRobsonJr <jrobsonjr16@gmail.com>
2019-11-19 22:52:01 -0300

    Update current obsession: "Everything Is Embarassing"

--- src/components/CurrentObsession.js
+++ src/components/CurrentObsession.js
@@ -8,13 +8,15 @@ const CurrentObsession = () => (
             <h2 className="text-light text-center">Current obsession</h2>
             <p className="text-light text-center m-0">
                 <i>
-                    Maniac, some may say that I'm a
+                    Could have been my anything / Now everything's embarrassing
                 </i>
             </p>
-            <p className="text-light text-center">— Conan Gray, Dan Nigro</p>
+            <p className="text-light text-center">
+                — Sky Ferreira, Ariel Rechtshaid, Devonté Hynes
+            </p>
             <iframe
                 className="responsive-iframe"
-                src="https://www.youtube.com/embed/-E-_IRJU5w0"
+                src="https://www.youtube.com/embed/_qsGZ6ngUoU"
                 frameborder="0"
                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen
