f838216727f2a60854478c394828f3fc8c3ea703
JRobsonJr <jrobsonjr16@gmail.com>
2020-01-29 21:59:06 -0300

    Move obsessions to a separate file to improve code

--- src/components/CurrentObsession.js
+++ src/components/CurrentObsession.js
@@ -2,28 +2,30 @@ import React from 'react';
 
 import './CurrentObsession.css';
 
-const CurrentObsession = () => (
-    <div className="current-obssession-container">
-        <div className="footer-content">
-            <h2 className="text-light text-center">Current obsession</h2>
-            <p className="text-light text-center m-0">
-                <i>
-                    I'm out of my head, and I know that you're scared, because
-                    hearts get broken
-                </i>
-            </p>
-            <p className="text-light text-center">
-                — Harry Styles, Kid Harpoon, Mitch Rowland, Tyler Johnson
-            </p>
-            <iframe
-                className="responsive-iframe"
-                src="https://www.youtube.com/embed/enuYFtMHgfU"
-                frameborder="0"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-            ></iframe>
+import obsessions from '../data/obsession-data';
+
+const CurrentObsession = () => {
+    const { lyricsExcerpt, songwriters, videoUrl } = obsessions[
+        obsessions.length - 1
+    ];
+    return (
+        <div className="current-obssession-container">
+            <div className="footer-content">
+                <h2 className="text-light text-center">Current obsession</h2>
+                <p className="text-light text-center m-0">
+                    <i>{lyricsExcerpt}</i>
+                </p>
+                <p className="text-light text-center">— {songwriters}</p>
+                <iframe
+                    className="responsive-iframe"
+                    src={videoUrl}
+                    frameborder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                ></iframe>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default CurrentObsession;
--- src/pages/obsessions.js
+++ src/pages/obsessions.js
@@ -2,48 +2,13 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
+import PostListItem from '../components/PostListItem';
 
 import { graphql } from 'gatsby';
 
-import './index.css';
-import PostListItem from '../components/PostListItem';
+import obsessions from '../data/obsession-data';
 
-const obsessions = [
-    {
-        dateInsertion: 'October 12, 2019',
-        lyricsExcerpt:
-            "I'm high enough from all the waiting to ride a wave on your inhaling",
-        songwriters: 'Imogen Heap, Guy Sighsworth (Frou Frou)',
-        videoUrl: 'https://www.youtube.com/embed/5XONarRhvI4',
-    },
-    {
-        dateInsertion: 'October 28, 2019',
-        lyricsExcerpt: "Maniac, some may say that I'm a",
-        songwriters: 'Conan Gray, Dan Nigro',
-        videoUrl: 'https://www.youtube.com/embed/-E-_IRJU5w0',
-    },
-    {
-        dateInsertion: 'November 19, 2019',
-        lyricsExcerpt:
-            "Could have been my anything / Now everything's embarrassing",
-        songwriters: 'Sky Ferreira, Ariel Rechtshaid, Devonté Hynes',
-        videoUrl: 'https://www.youtube.com/embed/_qsGZ6ngUoU',
-    },
-    {
-        dateInsertion: 'December 8, 2019',
-        lyricsExcerpt:
-            "But you just met somebody new / And now it's gon' be me and you",
-        songwriters: 'Robyn, Alexander Kronlund, Klas Åhlund',
-        videoUrl: 'https://www.youtube.com/embed/CHn7w3o22PA',
-    },
-    {
-        dateInsertion: 'December 29, 2019',
-        lyricsExcerpt:
-            "I'm out of my head, and I know that you're scared, because hearts get broken",
-        songwriters: 'Harry Styles, Kid Harpoon, Mitch Rowland, Tyler Johnson',
-        videoUrl: 'https://www.youtube.com/embed/enuYFtMHgfU',
-    },
-];
+import './index.css';
 
 const ObsessionHistory = () => {
     const obsessionList = obsessions
