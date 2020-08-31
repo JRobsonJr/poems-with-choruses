1e5d82c07b29f05f892ca49465b191d337080156
Robson Junior <jose.robson@splab.ufcg.edu.br>
2020-08-31 01:04:59 -0300

    Add titles to song obsessions

--- src/data/obsession-data.js
+++ src/data/obsession-data.js
@@ -4,12 +4,14 @@ const obsessions = [
         lyricsExcerpt:
             "I'm high enough from all the waiting to ride a wave on your inhaling",
         songwriters: 'Imogen Heap, Guy Sighsworth (Frou Frou)',
+        title: 'Breathe In',
         videoUrl: 'https://www.youtube.com/embed/5XONarRhvI4',
     },
     {
         dateInsertion: 'October 28, 2019',
-        lyricsExcerpt: "Maniac, some may say that I'm a",
+        lyricsExcerpt: "Maniac, some may say that I'm a...",
         songwriters: 'Conan Gray, Dan Nigro',
+        title: 'Maniac',
         videoUrl: 'https://www.youtube.com/embed/-E-_IRJU5w0',
     },
     {
@@ -17,6 +19,7 @@ const obsessions = [
         lyricsExcerpt:
             "Could have been my anything / Now everything's embarrassing",
         songwriters: 'Sky Ferreira, Ariel Rechtshaid, Devonté Hynes',
+        title: 'Everything Is Embarrassing',
         videoUrl: 'https://www.youtube.com/embed/_qsGZ6ngUoU',
     },
     {
@@ -24,6 +27,7 @@ const obsessions = [
         lyricsExcerpt:
             "But you just met somebody new / And now it's gon' be me and you",
         songwriters: 'Robyn, Alexander Kronlund, Klas Åhlund',
+        title: 'Call Your Girlfriend',
         videoUrl: 'https://www.youtube.com/embed/CHn7w3o22PA',
     },
     {
@@ -31,6 +35,7 @@ const obsessions = [
         lyricsExcerpt:
             "I'm out of my head, and I know that you're scared, because hearts get broken",
         songwriters: 'Harry Styles, Kid Harpoon, Mitch Rowland, Tyler Johnson',
+        title: 'Golden',
         videoUrl: 'https://www.youtube.com/embed/enuYFtMHgfU',
     },
     {
@@ -39,6 +44,7 @@ const obsessions = [
             "And when you decide it's your time to arrive, I've loved you for all of my life.",
         songwriters:
             'Halsey, Ammar Malik, Andrew VanWyngarden, Benny Blanco, Cashmere Cat, Dave Lubben, Kevin Snevely, Lido',
+        title: 'More',
         videoUrl: 'https://www.youtube.com/embed/MLw3JCQSkCU',
     },
     {
@@ -46,6 +52,7 @@ const obsessions = [
         lyricsExcerpt:
             "If you're offended by this song, you're clearly doing something wrong.",
         songwriters: 'Dua Lipa, Jason Evigan, Justin Tranter, Kennedi',
+        title: 'Boys Will Be Boys',
         videoUrl: 'https://www.youtube.com/embed/k0QWX2M7W7M',
     },
     {
@@ -53,6 +60,7 @@ const obsessions = [
         lyricsExcerpt:
             "And I won't say you're the reason I was on my knees, but I'm thinking 'bout making a comeback, back to me.",
         songwriters: 'Carly Rae Jepsen, Jack Antonoff, Jared Manierka',
+        title: 'Comeback',
         videoUrl: 'https://www.youtube.com/embed/CxI5C4Hel1o',
     },
     {
@@ -60,6 +68,7 @@ const obsessions = [
         lyricsExcerpt:
             "I didn't have it in myself to go with grace, and you're the hero flying around, saving face.",
         songwriters: 'Taylor Swift',
+        title: 'my tears ricochet',
         videoUrl: 'https://www.youtube.com/embed/OWbDJFtHl3w',
     },
 ];
