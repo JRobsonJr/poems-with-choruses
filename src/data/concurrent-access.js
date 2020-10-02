8576a373f66602da3aa098200e354b5b408a38be
Robson Junior <jose.robson@splab.ufcg.edu.br>
2020-10-02 17:57:22 -0300

    Add Concurrent Access data

---
+++ src/data/concurrent-access.js
@@ -0,0 +1,118 @@
+const concurrentAccess = {
+    id: 'concurrent-access.title',
+    playlist: {
+        title: 'Inspired by Concurrent Access',
+        url:
+            'https://open.spotify.com/playlist/1zmcKZw6SZyJJsQWJC74VE?si=Truv8dl5QNeO6qK3XYyO0g',
+    },
+    prologueUrl: '/concurrent-access-prologue',
+    sections: [
+        {
+            id: 'concurrent-access.side-a',
+            tracks: [
+                {
+                    number: 1,
+                    id: 'concurrent-access.track-1',
+                    title: 'Faith-Seeker',
+                    link: '/faith-seeker',
+                },
+                {
+                    number: 2,
+                    id: 'concurrent-access.track-2',
+                    title: 'All That Takes',
+                    link: '/all-that-takes',
+                },
+                {
+                    number: 3,
+                    id: 'concurrent-access.track-3',
+                    title: 'Worthy',
+                    link: '/worthy',
+                },
+                {
+                    number: 4,
+                    id: 'concurrent-access.track-4',
+                    title: 'Empty Corridors',
+                    link: '/empty-corridors',
+                },
+                {
+                    number: 5,
+                    id: 'concurrent-access.track-5',
+                    title: 'Say We Did It (The Comfort Out of the Blue)',
+                    link: '/say-we-did-it',
+                },
+            ],
+        },
+        {
+            id: 'concurrent-access.side-b',
+            tracks: [
+                {
+                    number: 6,
+                    id: 'concurrent-access.track-6',
+                    title: 'Overwrite',
+                    link: '/overwrite',
+                },
+                {
+                    number: 7,
+                    id: 'concurrent-access.track-7',
+                    title: 'If It Happens At All',
+                    link: '/if-it-happens-at-all',
+                },
+                {
+                    number: 8,
+                    id: 'concurrent-access.track-8',
+                    title: 'Go-To Me',
+                    link: '/go-to-me',
+                },
+                {
+                    number: 9,
+                    id: 'concurrent-access.track-9',
+                    title: 'YDWML!',
+                    link: '/ydwml!',
+                },
+                {
+                    number: 10,
+                    id: 'concurrent-access.track-10',
+                    title: 'Better Keep My Hopes',
+                    link: '/better-keep-my-hopes',
+                },
+            ],
+        },
+        {
+            id: 'concurrent-access.side-c',
+            tracks: [
+                {
+                    number: 11,
+                    id: 'concurrent-access.track-11',
+                    title: 'In Case You Ever Listen',
+                    link: '/in-case-you-ever-listen',
+                },
+                {
+                    number: 12,
+                    id: 'concurrent-access.track-12',
+                    title: "Now We're Talking",
+                    link: '/now-were-talking',
+                },
+                {
+                    number: 13,
+                    id: 'concurrent-access.track-13',
+                    title: 'Need You Back',
+                    link: '/need-you-back',
+                },
+                {
+                    number: 14,
+                    id: 'concurrent-access.track-14',
+                    title: "...Don't You?",
+                    link: '/dont-you',
+                },
+                {
+                    number: 15,
+                    id: 'concurrent-access.track-15',
+                    title: 'Revoked Access',
+                    link: '/revoked-access',
+                },
+            ],
+        },
+    ],
+};
+
+export default concurrentAccess;
